d489446c937e949247a958af1807ea4d006f8fb4
hedrall <masaharu73@gmail.com>
2024-05-19 19:42:41 +0900

    連続して開けるようにした

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,29 @@ use model::{board::Board, point::Point};
 
 fn main() {
     let mut board = Board::new();
-    board.print();
-    
-    // ユーザ入力を取る
-    let next_point = input::get_next_point();
 
-    match next_point {
-        Ok(point) => {
-            println!("{} {}", point.x, point.y);
-            board.open_cell(&point);
-            board.print();
-        },
-        Err(e) => match e {
-            InputError::Inquire(e) => println!("@@@ Inquier, {}", e),
-            InputError::Parse(e) => println!("@@@ Parse, {}", e),
-            InputError::InvalidValueCount => println!("@@@ Invalid Value Count, {}", e),
-        },
+    let mut ターンカウント = 0;
+
+    loop {
+        ターンカウント = ターンカウント + 1;
+        println!("ターン: {}", ターンカウント);
+        board.print();
+
+        // ユーザ入力を取る
+        let next_point = input::get_next_point();
+
+        match next_point {
+            Ok(point) => {
+                println!("{} {}", point.x, point.y);
+                board.open_cell(&point);
+            }
+            Err(e) => match e {
+                InputError::Inquire(e) => {
+                    panic!("@@@ Inquier, {}", e);
+                }
+                InputError::Parse(e) => println!("@@@ Parse, {}", e),
+                InputError::InvalidValueCount => println!("@@@ Invalid Value Count, {}", e),
+            },
+        }
     }
-
 }
